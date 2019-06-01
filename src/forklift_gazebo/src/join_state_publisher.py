7d6afbb61d980293235a871c72fcbe7cd0500c84
Your Name <you@example.com>
2019-06-01 14:24:29 +0530

    Lift operation working

--- src/forklift_gazebo/src/join_state_publisher.py
+++ src/forklift_gazebo/src/join_state_publisher.py
@@ -3,41 +3,48 @@ import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 
-def reset_model():
-    print("reset")
-    
-
 class Slider:
 
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("GUI Slider")
 
-        self.w1 = tk.Scale(self.root, label="Shift-right", length=500, from_=0, to=100, orient=tk.HORIZONTAL)
-        self.w1.pack()
-        self.w1.bind("<ButtonRelease-1>", self.updateValue_w1)
+        self.shift = tk.Scale(self.root, label="Shift", length=500, from_=0, to=100, orient=tk.HORIZONTAL)
+        self.shift.pack()
+        self.shift.bind("<ButtonRelease-1>", self.updateValue_shift)
+        self.shift.set(50)
 
-        self.w2 = tk.Scale(self.root, label="Shift-left", length=500, from_=0, to=100, orient=tk.HORIZONTAL)
-        self.w2.pack()
-        self.w2.bind("<ButtonRelease-1>", self.updateValue_w2)
+        self.lift = tk.Scale(self.root, label="Lift", length=200, from_=100, to=0, orient=tk.VERTICAL)
+        self.lift.pack()
+        self.lift.bind("<ButtonRelease-1>", self.updateValue_lift)
 
-        #self.b1 = tk.Button(text='Reset', command=reset_model).pack()
+        self.b1 = tk.Button(text='Reset', command=self.reset_model).pack()
 
         self.root.mainloop()
 
-    def updateValue_w1(self, event):
-        print("w1:{}".format(self.w1.get()))
-        self.w2.set(0)
-        self.join_state_publish("shift", (self.w1.get()*0.1)/100)
+    def updateValue_shift(self, event):
+        print("shift:{}".format(self.shift.get()))
+        if self.shift.get() >= 50:
+            self.join_state_publish("shift", ((self.shift.get()-50)*0.1)/50)
+        else:
+            self.join_state_publish("shift", ((self.shift.get()-50)*0.1)/50)
 
-    def updateValue_w2(self, event):
-        print("w2:{}".format(self.w2.get()))
-        self.w1.set(0)
-        self.join_state_publish("shift", (self.w2.get()*-0.1)/100)
+
+    def updateValue_lift(self, event):
+        print("lift:{}".format(self.lift.get()))
+        self.join_state_publish("lift", (self.lift.get()*0.25)/100)
+
+    def reset_model(self):
+        print("reset")
+        self.shift.set(50)
+        self.lift.set(0)
+        self.join_state_publish("shift", 0)
+        self.join_state_publish("lift", 0)
+
     
     def join_state_publish(self, joint_name, value):
         
-        #print("pub:{}, value:{}".format(joint_name, value))
+        print("pub:{}, value:{}".format(joint_name, value))
 
         pub = rospy.Publisher('joint_states', JointState, queue_size=10)
         rospy.init_node('joint_state_publisher')
